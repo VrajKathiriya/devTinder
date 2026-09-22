9529ec9806ad0dc695c2c6d3f0e1797d34abd158
vraj <vrajkathiriya2510@gmail.com>
2026-09-22 17:39:28 +0530

    auth middleware

--- src/app.js
+++ src/app.js
@@ -1,10 +1,34 @@
 const express = require("express");
+const { adminAuth, userAuth } = require("./middlewares/auth");
 
 const app = express();
 
-app.use("/hello", (req, res) => {
-  res.send("Hello, World!");
+app.use("/admin", adminAuth);
+
+app.get("/admin/getAllData", adminAuth, (req, res) => {
+  res.send("This is the getAllData route for admin.");
 });
+
+app.get("/user/login", (req, res) => {
+  res.send("This is the login route for user.");
+});
+
+app.get("/user/getAllData", userAuth, (req, res) => {
+  res.send("This is the getAllData route for user.");
+});
+
+app.get(
+  "/hello",
+  (req, res, next) => {
+    console.log("First callback function executed.");
+    // res.send("Hello, World!");
+    next();
+  },
+  (req, res) => {
+    console.log("Second callback function executed.");
+    res.send("This is the second callback function.");
+  },
+);
 
 app.listen(7777, () => {
   console.log("Server is running on port 7777");
---
+++ src/middlewares/auth.js
@@ -0,0 +1,21 @@
+const adminAuth = (req, res, next) => {
+  const header = "xyz";
+  const isAuthenticated = header === "xyz";
+  if (!isAuthenticated) {
+    return res.status(401).json({ message: "Unauthorized" });
+  } else {
+    next();
+  }
+};
+
+const userAuth = (req, res, next) => {
+  const header = "xyzz";
+  const isAuthenticated = header === "xyz";
+  if (!isAuthenticated) {
+    return res.status(401).json({ message: "Unauthorized" });
+  } else {
+    next();
+  }
+};
+
+module.exports = { adminAuth, userAuth };
