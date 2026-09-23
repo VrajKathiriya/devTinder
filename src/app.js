5be3da37b69b879380b617977bd42ad6eebaa83b
vraj <vrajkathiriya2510@gmail.com>
2026-09-23 16:18:03 +0530

    add validations

--- src/app.js
+++ src/app.js
@@ -6,18 +6,96 @@ const User = require("./models/user");
 
 const app = express();
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Virat",
-    lastName: "Kohli",
-    emailId: "vraj.kohli@example.com",
-    password: "password123",
-  });
+  console.log("hello", req.body);
+
   try {
+    // const existingUser = await User.findOne({ emailId: req.body.emailId });
+
+    // if (existingUser) {
+    //   return res.status(409).send("User with this email already exists");
+    // }
+
+    const user = new User(req.body);
     await user.save();
     res.send("User created successfully");
   } catch (error) {
     res.status(500).send("Error creating user: " + error.message);
+  }
+});
+
+app.get("/user", async (req, res) => {
+  try {
+    const user = await User.find({ emailId: req.body.emailId });
+
+    if (user.length === 0) {
+      return res.status(404).send("User not found");
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(400).send("Error fetching user: " + error.message);
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).send("Error fetching users: " + error.message);
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.body.userId);
+
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    res.send("User deleted successfully");
+  } catch (error) {
+    res.status(500).send("Error deleting user: " + error.message);
+  }
+});
+
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params.userId;
+  const data = req.body;
+
+  const ALLOWED_FIELDS = [
+    "userId",
+    "age",
+    "gender",
+    "photoUrl",
+    "about",
+    "skills",
+  ];
+
+  try {
+    const isUpdateAllowed = Object.keys(data).every((field) =>
+      ALLOWED_FIELDS.includes(field),
+    );
+    if (!isUpdateAllowed) {
+      throw new Error("Update not allowed");
+    }
+    if (data?.skills.length > 10) {
+      throw new Error("Skills cannot be more than 10");
+    }
+    const user = await User.findByIdAndUpdate(userId, data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+
+    console.log("update user", user);
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    res.send("User updated successfully1");
+  } catch (error) {
+    res.status(500).send("Error updating user: " + error.message);
   }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,28 +1,50 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      validate: {
+        validator: function (value) {
+          return ["male", "female", "other"].includes(value.toLowerCase());
+        },
+        message: (props) => `${props.value} is not a valid gender`,
+      },
+    },
+    photoUrl: {
+      type: String,
+    },
+    about: {
+      type: String,
+      default: "This is default about section.",
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-    required: true,
-  },
-  emailId: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  { timestamps: true },
+);
 
 module.exports = mongoose.model("User", userSchema);
